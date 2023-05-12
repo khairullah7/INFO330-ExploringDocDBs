ec2f6fd6d1e1be144cd3825f98200011175d49ab
khairullah7 <129241013+khairullah7@users.noreply.github.com>
2023-05-11 17:38:53 -0700

    Update import.py

--- import.py
+++ import.py
@@ -1,10 +1,11 @@
-import pymongo
- from pymongo import MongoClient
-    mongoClient = MongoClient("mongodb://localhost/pokemon.csv")
-     pokemonDB = mongoClient['pokemondb']
-     pokemonColl = pokemonDB['pokemon_data']
+from pymongo import MongoClient
+mongoClient = MongoClient("mongodb://localhost/pokemon.csv")
+pokemonDB = mongoClient["pokemondb"]
+pokemonColl = pokemonDB["pokemon_data"]
 
-     pikachuquery = { "name": "Pikachu" }
-     pikachu = pokemonColl.find_one(pikachuquery)
-    
-      print("I found " + str(pokemonColl.count_documents({})) + " pokemon")
+myquery = {"name" : "Pikachu"}
+pikachu = pokemonColl.find(myquery)
+
+for x in pikachu:
+    print(x)
+
